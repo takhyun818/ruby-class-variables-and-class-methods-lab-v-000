188b9be213d2d5998956070891d1eb0d61c4c5a2
Tak H Kim <takhyun818@gmail.com>
2018-11-12 00:00:36 +0000

    automatically backed up by learn

---
+++ lib/song.rb
@@ -0,0 +1,27 @@
+class Song
+  attr_accessor :name, :artist, :genre
+    @@count = 0
+    @@artists = []
+    @@genres = []
+
+    def initialize(name, artist, genre)
+		@name = name
+		@artist = artist
+		@genre = genre
+		@@count += 1
+		@@genres << genre
+		@@artists << artist
+	end
+
+	def self.count
+		@@count
+	end
+
+	def self.artists
+		@@artists.uniq
+	end
+
+	def self.genres
+		@@genres.uniq
+	end
+end
