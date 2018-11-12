dd5a732973d5b9ebca7a1d28631c2e2e61fe5870
Tak H Kim <takhyun818@gmail.com>
2018-11-12 00:01:29 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -24,13 +24,12 @@ class Song
 	def self.genres
 		@@genres.uniq
 	end
-end
 
 def self.genre_count
   genre_count = {}
   @@genres.each do |genre|
     if genre_count[genre]
-      genre_count[genre] += 1 
+      genre_count[genre] += 1
     else
       genre_count[genre] = 1
     end
@@ -42,11 +41,11 @@ def self.artist_count
   artist_count = {}
   @@artists.each do |artist|
       if artist_count[artist]
-          artist_count[artist] += 1 
+          artist_count[artist] += 1
       else
          artist_count[artist] = 1
       end
   end
   artist_count
 end
-end
+
